881e2f6faf96d9eda6ba3171a8ae7e90fe63e548
Renan Le Gall <renan.legall@ieseg.fr>
2017-10-05 17:13:29 +0200

    iniatialization and first designs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,32 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function(){
+
+  $(window).on("scroll", function(){
+    if ( $(this).scrollTop() > 80 ){
+      $(".navbar").css({"background": "rgba(0, 0, 0, 0.7)",
+        "box-shadow": "0 0 2px black"});
+    } else {
+      $(".navbar").css({
+        "background": "transparent",
+        "box-shadow": "0 0 0px transparent"
+      });
+    }
+  });
+
+  $('a[href*=#]:not([href=#])').click(function() {
+     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+       var target = $(this.hash);
+       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+       if (target.length) {
+         $('html,body').animate({
+           scrollTop: target.offset().top
+         }, 1000);
+         return false;
+       }
+     }
+   });
+
+});
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :pricing, :contact]
 
   def home
+  end
+
+  def pricing
+  end
+
+  def contact
   end
 end
